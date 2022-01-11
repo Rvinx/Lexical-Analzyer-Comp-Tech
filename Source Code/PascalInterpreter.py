67124ecbb47d60762675c9bec97bae5b8f606589
Rvinx <rvinxpadz456@gmail.com>
2022-01-11 22:51:49 +0700

    Update PascalInterpreter.py

--- Source Code/PascalInterpreter.py
+++ Source Code/PascalInterpreter.py
@@ -14,9 +14,42 @@ class PascalInterpreter:
             # pusyang
         }
 
-    def collect_data(self):
-        self.data = []
+    # def collect_data(self):
+    #     self.data = []
+    #     for lineno in self.stat:
+    #         if self.prog[lineno][0] == 'DATA':
+    #             self.data = self.data + self.prog[lineno][1]
+    #     self.dc = 0
+
+    def check_end(self):
+        has_end = 0
         for lineno in self.stat:
-            if self.prog[lineno][0] == 'DATA':
-                self.data = self.data + self.prog[lineno][1]
-        self.dc = 0
+            if self.prog[lineno][0] == 'END.' and not has_end:
+                has_end = lineno
+        if not has_end:
+            print("NO END INSTRUCTION")
+            self.error = 1
+            return
+        if has_end != lineno:
+            print("END IS NOT LAST")
+            self.error = 1
+        
+    # Check loops
+    # nanti
+
+    # Evaluate an expression
+    # nanti
+
+    # Evaluate a relational expression
+    # nanti
+
+    # Assignment
+    # nanti
+
+    # Change the current line number
+    # nanti
+
+    # Run
+    def run(self):
+        self
+    
